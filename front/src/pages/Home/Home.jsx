effef51c88e39482790dbd83e604ac81a41f745d
Edu <luis_100404@hotmail.com>
2025-01-08 14:04:11 -0300

    add updates in archives

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,9 +1,45 @@
 import Navbar from "../../components/Navbar/Navbar"
+import PDF from "../../../Assets/curriculoLC.docx.pdf";
+import IMG from "../../../Assets/imgLC.png";
+import { Link } from "react-router-dom";
+
+import Style from "./Home.module.css";
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+
+      <div className={Style.titlePrinc}>
+          <div>
+           <div className={Style.titleName}>
+              
+              <div>
+                <h1>Lucas Cauã</h1>
+                <h1>Sabino</h1>
+              </div>
+
+              <h2>Desenvolvedor Back-end</h2>
+            
+            </div>
+
+            <div className={Style.titleCV}>
+              <a href={PDF} download="curriculoLC.docx.pdf">Download CV</a>
+              <Link to="mailto:lucas_sbn@outlook.com">Entrar em contato</Link>
+            </div>
+
+          </div>
+
+          <div>
+
+            <div className={Style.divImg}>
+              <img src={IMG}/>
+            </div>
+
+
+          </div>
+      </div>
+
     </div>
   )
 }
--- front/src/pages/Skills/Skills.jsx
+++ front/src/pages/Skills/Skills.jsx
@@ -1,9 +1,100 @@
 import Navbar from "../../components/Navbar/Navbar"
 
+///icons
+import { FaCss3Alt } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaPython } from "react-icons/fa";
+import { SiFlask } from "react-icons/si";
+import { SiDjango } from "react-icons/si";
+import { IoLogoFirebase } from "react-icons/io5";
+import { GrMysql } from "react-icons/gr";
+import { FaGitAlt } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { SiExpress } from "react-icons/si";
+import { SiPandas } from "react-icons/si";
+
+//STYLE
+import Style from "./Skills.module.css"
+
 const Skills = () => {
     return (
-        <div>
+      <div>
         <Navbar/>
+
+        <h1 className={Style.h1Skills}>Skills</h1>
+
+        <div className={Style.linguagens}>
+
+          <div className={Style.html}>
+            <FaHtml5/>
+            <h3>HTML5</h3>
+          </div>
+
+          <div className={Style.css}>
+            <FaCss3Alt/>
+            <h3>CSS3</h3>
+          </div>
+
+
+          <div className={Style.javascript}>
+            <IoLogoJavascript/>
+            <h3>Javascript</h3>
+          </div>
+
+          <div className={Style.firebase}>
+            <IoLogoFirebase/>
+            <h3>Firebase</h3>
+          </div>
+
+          <div className={Style.python}>
+            <FaPython/>
+            <h3>Python</h3>
+          </div>
+
+          <div className={Style.pandas}>
+            <SiPandas/>
+            <h3>Pandas</h3>
+          </div>
+
+          <div className={Style.flask}>
+            <SiFlask/>
+            <h3>Flask</h3>
+          </div>
+
+          <div className={Style.django}>
+            <SiDjango/>
+            <h3>Django</h3>
+          </div>
+
+          <div className={Style.node}>
+            <FaNodeJs/>
+            <h3>Node</h3>
+          </div>
+
+          <div className={Style.mysql}>
+            <GrMysql/>
+            <h3>MySQL</h3>
+          </div>
+
+          <div className={Style.express}>
+            <SiExpress/>
+            <h3>Express</h3>
+          </div>
+
+          <div className={Style.github}>
+            <FaGithub/>
+            <h3>GitHub</h3>
+          </div>
+
+          <div className={Style.git}>
+            <FaGitAlt/>
+            <h3>Git</h3>
+          </div>
+
+          </div>
+
     </div>
   )
 }
